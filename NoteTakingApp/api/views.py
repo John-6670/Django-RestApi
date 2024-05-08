f92b7bb313a78fa888f7c93f6988ec658ae5b433
John_6670 <mayn2005m@gmail.com>
2024-05-08 13:52:40 +0330

    added error handling to views.py

--- NoteTakingApp/api/views.py
+++ NoteTakingApp/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import NoteSerializers
 from .models import Note
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import PermissionDenied, ValidationError, NotFound
 
 
 # Create your views here.
@@ -11,15 +11,24 @@ class NoteListCreate(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
+        if not user.is_authenticated:
+            raise PermissionDenied("You need to be logged in to view notes.")
+        
         return Note.objects.filter(author=user)
     
     def perform_create(self, serializer):
         user = self.request.user
-        if serializer.is_valid():
-            serializer.save(author=user)
+        if not serializer.is_valid():
+            raise ValidationError("Invalid data.")
+        
+        serializer.save(author=user)
 
-    def delete(self, request, *args, **kwargs):
-        self.get_queryset().delete()
+    def delete(self):
+        queryset = self.get_queryset()
+        if not queryset.exists():
+            raise NotFound("No notes found to delete.")
+        
+        queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -30,5 +39,7 @@ class NoteRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         user = self.request.user
+        if not user.is_authenticated:
+            raise PermissionDenied("You need to be logged in to view notes.")
+        
         return Note.objects.filter(author=user)
-    
